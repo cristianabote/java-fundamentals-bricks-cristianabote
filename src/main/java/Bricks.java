928b641465793cbb22fdff92e6951057dc013581
cristianabote <cristianamihaelaplesa@gmail.com>
2021-03-14 14:18:58 +0200

    Final commit

--- src/main/java/Bricks.java
+++ src/main/java/Bricks.java
@@ -1,12 +1,25 @@
+import java.util.Scanner;
+
 public class Bricks {
     public static void main(String[] args)
     {
-        int i,n=30;
-        System.out.println("Numarul de caramizi n=" + n);
-        // cin>>n;
-        for(i=1;n-3*i>0;i++)
-            n=n-3*i;
-        if(n-i<=0)
+        //Patlu and Motu want to build a house and they have to put some number of bricks n from one place to another.
+        // They decided , that the one who puts the last brick would win a dinner.
+        //They have to follow a simple rule: in the i'th round, Patlu puts i bricks whereas Motu puts ix2 bricks.
+        //There are only n bricks, who put the last brick.
+        //
+        //Input:
+        //number of bricks n
+        //
+        //Output:
+        //Output "Patlu" (without the quotes) if Patlu puts the last bricks ,"Motu"(without the quotes) otherwise.
+        int i,numberOfBricks;
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Introduceti numarul de caramizi: " );
+        numberOfBricks=scan.nextInt();
+        for(i=1;numberOfBricks-3*i>0;i++)
+            numberOfBricks=numberOfBricks-3*i;
+        if(numberOfBricks-i<=0)
             System.out.println("Patlu wins");
         else
             System.out.println("Motu wins");
