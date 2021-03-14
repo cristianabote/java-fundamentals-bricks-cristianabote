9172f1dd2c461ce0479a2ed363e54393f0b96940
cristianabote <cristianamihaelaplesa@gmail.com>
2021-03-14 14:28:57 +0200

    Final commit

--- src/main/java/Bricks.java
+++ src/main/java/Bricks.java
@@ -7,10 +7,8 @@ public class Bricks {
         // They decided , that the one who puts the last brick would win a dinner.
         //They have to follow a simple rule: in the i'th round, Patlu puts i bricks whereas Motu puts ix2 bricks.
         //There are only n bricks, who put the last brick.
-        //
         //Input:
-        //number of bricks n
-        //
+        //number of bricks 
         //Output:
         //Output "Patlu" (without the quotes) if Patlu puts the last bricks ,"Motu"(without the quotes) otherwise.
         int i,numberOfBricks;
